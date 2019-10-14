78e9a9551aa5b4434d0cba56d904b7fca32c9cc4
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-14 01:10:17 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -30,11 +30,11 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
-  count = 0
+  sum = 0
   src.each do |array|
     if array[0] % 2 == 0 || array[1] % 2 == 0 
-      count[array]+=1
+      sum = array[0] + array[1]
     end
   end
-    return count 
+    return sum
 end
