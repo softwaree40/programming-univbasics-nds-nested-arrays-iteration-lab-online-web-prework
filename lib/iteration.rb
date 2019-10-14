5ab18b4f3bd789c32ef6e0e59c83c24af86340a4
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-14 01:30:07 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,17 +1,25 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+    new_array = []
+    src.each do |array|
+    new_array.push("I love #{array[0]} and #{array[1]} on my pizza")
+     
+    end
+   return new_array 
 end
 
 def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
+  new_array = []
+  src.each  do |array|
+    if array[0] > array[1]
+      new_array << array[0]
+    else
+      new_array << array[1]
+    end
+  end
+   return new_array
 end
 
 def total_even_pairs(src)
@@ -22,4 +30,13 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
+   sum = 0
+  src.each do |array|
+    if array[0] % 2 == 0 && array[1] % 2 == 0
+      
+      sum+=array[0] + array[1]
+      
+    end
+  end
+    return sum
 end
